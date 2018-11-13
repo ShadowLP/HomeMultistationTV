55fe55e80e70cfe63940f644b36ad9772aaf9bf8
Гущин Роман Игоревич <romanshadow3@gmail.com>
2018-11-13 15:55:32 +0400

    Initial commit

---
+++ src/DesktopApp/Windows/HomeMultistationTV.WindowsDesktop.Infrastruct/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HomeMultistationTV.WindowsDesktop.Infrastruct
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Services/Weather/Weather.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Weather.Domain
+{
+    public class Class1
+    {
+    }
+}
